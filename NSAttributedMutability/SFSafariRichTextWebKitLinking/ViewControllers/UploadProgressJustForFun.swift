9ebc4f6c96126bf204df1154889d954a4acf1a7c
Jerry Ren <jerryren.ios@gmail.com>
2020-11-19 23:29:12 -0500

    borrorrorrowed

--- NSAttributedMutability/SFSafariRichTextWebKitLinking/ViewControllers/UploadProgressJustForFun.swift
+++ NSAttributedMutability/SFSafariRichTextWebKitLinking/ViewControllers/UploadProgressJustForFun.swift
@@ -38,4 +38,49 @@ class ImgUploaderProgressiveViewController: UIViewController, UIImagePickerContr
     @IBOutlet weak var imageUploadProgressView: UIProgressView!
     @IBOutlet weak var progressLabel: UILabel!
     @IBOutlet weak var uploadButton: UIButton!
+    
+    //    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject])
+    //    {
+    //        myImageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+    //        myImageView.backgroundColor = UIColor.clear
+    //        self.dismiss(animated: true, completion: nil)
+    //        uploadImage()
+    //    }
+    //
+    //    func uploadImage()
+    //    {
+    //        let imageData = UIImageJPEGRepresentation(myImageView.image, 1)
+    //        if(imageData == nil ) { return }
+    //        self.uploadButton.enabled = false
+    //
+    //        let uploadScriptUrl = NSURL(string:"https://www.freewebmentor.com/apiscript")
+    //        var request = NSMutableURLRequest(URL: uploadScriptUrl!)
+    //        request.HTTPMethod = "POST"
+    //        request.setValue("Keep-Alive", forHTTPHeaderField: "Connection")
+    //
+    //        var configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
+    //        var session = NSURLSession(configuration: configuration, delegate: self, delegateQueue: NSOperationQueue.mainQueue())
+    //
+    //        var task = session.uploadTaskWithRequest(request, fromData: imageData)
+    //        task.resume()
+    //    }
+    //
+    //    func URLSession(session: URLSession, task: URLSessionTask, didCompleteWithError error: NSError?)
+    //    {
+    //        println("didCompleteWithError")
+    //        let myAlert = UIAlertView(title: "Alert", message: error?.localizedDescription, delegate: nil, cancelButtonTitle: "OK")
+    //        myAlert.show()
+    //        self.uploadButton.enabled = true
+    //    }
+    
+    //    func URLSession(session: NSURLSession, dataTask: NSURLSessionDataTask, didReceiveResponse response: NSURLResponse, completionHandler: (NSURLSessionResponseDisposition) -> Void)
+    //    {
+    //        println("didReceiveResponse")
+    //        println(response);
+    //        self.uploadButton.enabled = true
+    //    }
+    //    func URLSession(session: NSURLSession, dataTask: NSURLSessionDataTask, didReceiveData data: NSData)
+    //    {
+    //        println("didReceiveData")
+    //    }
 }
