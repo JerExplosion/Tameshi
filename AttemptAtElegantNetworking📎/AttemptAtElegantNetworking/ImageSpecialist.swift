68741f734e23ef968e7ed278c58c240e52c2559f
Jerry Ren <jerryren.ios@gmail.com>
2020-12-15 23:38:27 -0500

    <UIImage?, Never>AnyPublisher

--- AttemptAtElegantNetworking📎/AttemptAtElegantNetworking/ImageSpecialist.swift
+++ AttemptAtElegantNetworking📎/AttemptAtElegantNetworking/ImageSpecialist.swift
@@ -8,9 +8,19 @@
 import UIKit
 import Combine
 
+extension UIImageView {
+    // 瞎子摸鱼
+    func imageLoadingCombineWay(targetUrl: URL) -> AnyPublisher<UIImage?, Never> {
+        return URLSession.shared.dataTaskPublisher(for: targetUrl)
+            .map { (data, _) -> UIImage? in return UIImage(data: data) }
+            .catch { ero in return Just(nil) }
+        .subscribe(on: DispatchQueue.global()) //background queue
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
+}
 
-
-
+ 
 
 
 
