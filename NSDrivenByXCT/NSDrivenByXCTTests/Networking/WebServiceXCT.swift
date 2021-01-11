9fcc91d4514f92103a75eed09adb9d40956979e7
Jerry Ren <jerryren.ios@gmail.com>
2021-01-11 16:43:58 -0500

    abnormalJsonResponse' case.debugged&passed

--- NSDrivenByXCT/NSDrivenByXCTTests/Networking/WebServiceXCT.swift
+++ NSDrivenByXCT/NSDrivenByXCTTests/Networking/WebServiceXCT.swift
@@ -12,7 +12,7 @@ class WebServiceXCT: XCTestCase {
     
     var sUpRequestModel: SUpRequestModel? 
     var sut: SupWebService?
-
+    
     override func setUpWithError() throws {
         let ephConfig = URLSessionConfiguration.ephemeral
         ephConfig.protocolClasses = [UrlMockedProtocolClass.self]
@@ -27,21 +27,37 @@ class WebServiceXCT: XCTestCase {
         sUpRequestModel = nil
         UrlMockedProtocolClass.stubbedResponseData = nil
     }
-
+    
     func testingWSwithSuccessfulResponse() throws {
+        let jsonStr = "{\"status\":\"oKks666比\"}"
+        UrlMockedProtocolClass.stubbedResponseData = jsonStr.data(using: .utf8)
         
-        let jsonStr = "{\"status\":\"ok\"}"
- //       UrlMockedProtocolClass.stubbedResponseData = jsonStr.data(using: .utf8)
-
-        let expectation = self.expectation(description: "sUp web service expectation")
-         
+        let expectation = self.expectation(description: "expectation for a plain vanilla successful response")
+        
         guard let sUpRequestModel = sUpRequestModel else { throw SUpErrors.requestModelErro }
         sut?.sUp(with: sUpRequestModel) { (responseModel, error) in
-            // Assert
-            XCTAssertEqual(responseModel?.status, "ok") // or "oK"
+            
+            XCTAssertEqual(responseModel?.status, "oKks666比")
             expectation.fulfill()
         }
         self.wait(for: [expectation], timeout: 5)
+    }
+    
+      
+private func testingWwithAbnormalResponse() {
+    
+        let diffJsonStr = "{\"path\":\"/users\", \"error\":\"Internal Server Error\"}"
+        UrlMockedProtocolClass.stubbedResponseData = diffJsonStr.data(using: .utf8)
+        
+        let expectation = self.expectation(description: "expectation for an abnormal Json structure returned")
+        
+        guard let sUpRequestModel = sUpRequestModel else { return }
+        sut?.sUp(with: sUpRequestModel) { (responseModel, erro) in
+            XCTAssertNil(responseModel, "the response model should be nil in this scenario")
+            XCTAssertEqual(erro, SUpErrors.parsingErro, "the expected erro is not returned")
+            expectation.fulfill()
+        }
+        self.wait(for: [expectation], timeout: 5.2)
     }
 }
 
